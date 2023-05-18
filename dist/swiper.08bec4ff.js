8f1c14309834f519b51eb8285cfea637c103a201
choiyeonjeong <cyj121585@gmail.com>
2023-05-18 17:57:12 +0900

    0518

--- dist/swiper.08bec4ff.js
+++ dist/swiper.08bec4ff.js
@@ -226,7 +226,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49523" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51696" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
