68d72536dc56ca173fc05cbc70698ed00b86832d
choiyeonjeong <cyj121585@gmail.com>
2023-05-23 15:46:37 +0900

    main

--- js/main.js
+++ js/main.js
@@ -117,3 +117,82 @@ for (let i = 0; i < livingImg.length; i++) {
         tableMain.src = './images/issue_table' + (i + 1) + '.png';
     };
 }
+
+/* -------------instagram--------------- */
+//롤링 배너 복제본 생성
+let roller = document.querySelector('.roller');
+roller.id = 'roller1';
+
+let clone = roller.cloneNode(true);
+clone.id = 'roller2';
+document.querySelector('.insta_box').appendChild(clone); //부착
+
+//원본, 복제본 배너 위치 지정
+document.querySelector('#roller1').style.left = '0px';
+document.querySelector('#roller2').style.left = document.querySelector('.roller ul').offsetWidth + 'px';
+
+//클래스 할당
+roller.classList.add('original');
+clone.classList.add('clone');
+
+//인터벌 메서드로 애니메이션 생성
+let rollerWidth = document.querySelector('.roller ul').offsetWidth; //회전 배너 너비값
+let betweenDistance = 1; //이동 크기 - 정수여야 함
+
+//롤링 시작
+function startRoller() {
+    originalID = window.setInterval(betweenRollCallback, parseInt(1000 / 100), betweenDistance, document.querySelector('#roller1'));
+    cloneID = window.setInterval(betweenRollCallback, parseInt(1000 / 100), betweenDistance, document.querySelector('#roller2'));
+}
+
+//롤링 정지
+function stopRoller() {
+    clearInterval(originalID);
+    clearInterval(cloneID);
+}
+
+//마우스 호버시 롤링이 멈추었다 벗어나면 다시 롤링이 되도록 처리
+document.getElementById('roller1').addEventListener('mouseover', () => {
+    stopRoller();
+});
+document.getElementById('roller2').addEventListener('mouseover', () => {
+    stopRoller();
+});
+document.getElementById('roller1').addEventListener('mouseout', () => {
+    startRoller();
+});
+document.getElementById('roller2').addEventListener('mouseout', () => {
+    startRoller();
+});
+
+//인터벌 애니메이션 함수(공용)
+function betweenRollCallback(d, roller) {
+    let left = parseInt(roller.style.left);
+    roller.style.left = left - d + 'px'; //이동
+    //조건부 위치 리셋
+    if (rollerWidth + (left - d) <= 0) {
+        roller.style.left = rollerWidth + 'px';
+    }
+}
+
+startRoller(); //롤링 초기화
+
+/* txt_box */
+const upBtn = document.querySelector('.info_up');
+const downBtn = document.querySelector('.info_down');
+const txtBox = document.querySelector('.text_box');
+
+downBtn.addEventListener('click', slideDown);
+upBtn.addEventListener('click', slideUp);
+
+function slideDown() {
+    upBtn.style.display = 'inline-block';
+    downBtn.style.display = 'none';
+    txtBox.classList.add('ani');
+}
+
+function slideUp() {
+    upBtn.style.display = 'none';
+    downBtn.style.display = 'inline-block';
+    txtBox.classList.remove('ani');
+}
